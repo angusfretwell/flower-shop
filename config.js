0795e89cfc3579e642fc8affdcd80e658f27e1f5
Angus Fretwell <angusfretwell@me.com>
2017-04-29 21:07:00 +1000

    feat: add products config file

---
+++ config.js
@@ -0,0 +1,37 @@
+exports.PRODUCTS = [{
+	name: 'Roses',
+	code: 'R12',
+	bundles: [{
+		quantity: 5,
+		price: 699
+	}, {
+		quantity: 10,
+		price: 1299
+	}]
+}, {
+	name: 'Lilies',
+	code: 'L09',
+	bundles: [{
+		quantity: 3,
+		price: 995
+	}, {
+		quantity: 6,
+		price: 1695
+	}, {
+		quantity: 9,
+		price: 2495
+	}]
+}, {
+	name: 'Tulips',
+	code: 'T58',
+	bundles: [{
+		quantity: 3,
+		price: 595
+	}, {
+		quantity: 5,
+		price: 995
+	}, {
+		quantity: 9,
+		price: 1699
+	}]
+}];
