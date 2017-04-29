883d8928e5fa2c7bb05f9c96bfc187bf097ace25
Angus Fretwell <angusfretwell@me.com>
2017-04-29 21:27:24 +1000

    feat: implement validateOrderItem

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ exports.getItem = () => {
 exports.getProductByCode = code =>
 	_.find(PRODUCTS, _.matchesProperty('code', code));
 
-exports.validateOrderItem = () => {
-	//
-};
+exports.validateOrderItem = (product, quantity) =>
+	!_(product.bundles)
+		.sortBy('quantity')
+		.reverse()
+		.reduce((remainder, bundle) => remainder % bundle.quantity, quantity);
 
 exports.getItemBundles = () => {
 	//
